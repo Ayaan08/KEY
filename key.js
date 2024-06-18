03d7cd29908b40cf48a51db13b89c6a5a7f0f5c4
Ayaan08 <85597340+Ayaan08@users.noreply.github.com>
2024-06-19 00:50:04 +0530

    Update key.js

--- key.js
+++ key.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const port = 5000;
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 let claimedKeys = new Set();
 
@@ -29,6 +31,10 @@ app.post('/validate_key', (req, res) => {
     } else {
         res.json({ valid: false });
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
